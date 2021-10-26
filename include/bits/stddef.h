bc3fc902c94c6ea870db9bf7fc48b1f452257405
learnforpractice <learnforpractice@gmail.com>
2021-10-26 19:27:28 +0800

    fix redefine error

--- include/bits/stddef.h
+++ include/bits/stddef.h
@@ -6,8 +6,9 @@
 #else
 #ifdef __wasm__
    typedef unsigned long size_t;
+   typedef long ptrdiff_t; //XXX intptr_t
 #else
    typedef unsigned int size_t;
-#endif
    typedef int ptrdiff_t; //XXX intptr_t
+#endif
 #endif
